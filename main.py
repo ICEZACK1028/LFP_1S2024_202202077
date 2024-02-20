aa7bf2a3557d37298ab1e4de19a80e5f021c50ec
Isaac <2010825510101@ingenieria.usac.edu.gt>
2024-02-19 23:57:17 -0600

    Creación de archivo main y menús

---
+++ main.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import filedialog
+from Programa import Programa
+
+def cargar_archivo(extension):
+    root = tk.Tk(); root.attributes('-alpha',0.01)
+    root.attributes('-topmost',True)
+    root.tk.eval(f'tk::PlaceWindow {root._w} center')
+    root.withdraw()
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                            title = "Select a File", 
+                                            filetypes=[(f"{extension} files", f"*.{extension}")])
+    
+    if not filename:  
+        root.destroy()
+        print('Cancelado por el usuario')
+        return None
+    
+    root.destroy()
+    return filename 
+    
+def menu():
+    programa = Programa()
+    while True:
+        print("\nMenú Principal")
+        print("1. Cargar Estudiantes")
+        print("2. Ingresar Calificaciones")
+        print("3. Reporte general de estudiantes")
+        print("4. Reporte de aprobación de estudiantes")
+        print("5. Top 3 estudiantes con mejor promedio")
+        print("6. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == '1':
+            filename = cargar_archivo('est')
+            if filename:
+                programa.cargar_estudiantes(filename)
+        elif opcion == '2':
+            filename = cargar_archivo("cali")
+            if filename:
+                programa.ingresar_calificaciones(filename)
+        elif opcion == '3':
+            programa.listar_estudiantes()
+            programa.generar_reporte_general()
+        elif opcion == '4':
+            programa.generar_reporte_aprobacion()
+        elif opcion == '5':
+            programa.generar_top_tres()
+        elif opcion == '6':
+            print("Saliendo...")
+            break
+        else:
+            print("Opción inválida. Intente de nuevo.")
+
+if __name__ == "__main__":
+    menu()
