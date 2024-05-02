2bec205524c6f128859c1976fd8a77d146097b60
Isaac <2010825510101@ingenieria.usac.edu.gt>
2024-05-02 06:09:18 -0600

    Se agrega la generación del árbol de derivación

--- Proyecto_2/classes/main.py
+++ Proyecto_2/classes/main.py
@@ -1,6 +1,5 @@
 from ventana import GUI
 from convertidor_html import ConvertidorHTML
-from convertidor_json import ConvertidorJSON
 
 if __name__ == "__main__":
     app = GUI()
--- Proyecto_2/classes/ventana.py
+++ Proyecto_2/classes/ventana.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 import webbrowser
-from tkinter import filedialog
+from tkinter import Toplevel, ttk, filedialog
 from analizador import Analizador
 from convertidor_html import ConvertidorHTML
-from convertidor_json import ConvertidorJSON
 from Parser import Parser
 from Analizador_expresiones import Analizador_expresiones
 import graphviz
@@ -22,12 +21,6 @@ class GUI(tk.Tk):
         title.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
 
         self.insert_line()
-
-        # input_label = tk.Label(self, text="Texto de Entrada:", font=("Arial", 12, "bold"), fg="#0F2B46", bg="#F5F7F8")
-        # input_label.grid(row=3, column=0, sticky="w", padx=10, pady=5)
-
-        # output_label = tk.Label(self, text="Texto de Salida:", font=("Arial", 12, "bold"), fg="#0F2B46", bg="#F5F7F8")
-        # output_label.grid(row=3, column=1, sticky="w", padx=10, pady=5)
 
         self.input_text = tk.Text(self, height=35, width=88, highlightbackground='gray90', highlightthickness=2, relief=tk.FLAT)
         self.input_text.grid(row=4, column=0, padx=10, pady=5)
@@ -120,10 +113,14 @@ class GUI(tk.Tk):
             texto_error_sintáctico = 'Errores sintácticos:\n'
             for error in error_sintacticos:
                 texto_error_sintáctico += f'Error: {error.value}. Línea: {error.line}. Columna: {error.column}\n'
+        
+            self.output_text = tk.Text(self, height=35, width=88, highlightbackground='gray90', highlightthickness=2, relief=tk.FLAT)
+            self.output_text.grid(row=4, column=1, padx=10, pady=5)
             self.output_text.delete(1.0, tk.END)
             self.output_text.insert(tk.END, f'No se pudo analizar el archivo.\n{texto_error} \n{texto_error_sintáctico}')
         else:
             resultados = analizador_expresiones.evaluar_expresiones_cadenas(expresiones_cadena)
+            self.output_text.grid_remove()
             self.mostrar_resultados(resultados)
         
         convertidor_html.generar_reporte_tokens(tokens_lexemas)
@@ -137,58 +134,75 @@ class GUI(tk.Tk):
         filename = 'D:/USAC/5to. SEMESTRE/LENGUAJES FORMALES Y DE PROGRAMACIÓN/Práctica 1/Proyecto_2/reports/Reporte_Errores.html'
         webbrowser.open('file://' + filename, new=2)
 
-    def generar_arbol_derivacion(self):
-        pass  # Implementa la lógica para generar el reporte de árbol de derivación
-
     def mostrar_resultados(self, resultados):
         # Encabezados de la tabla
         table_header = ["ID", "Expresión Regular", "Cadena", "Cumple"]
 
         # Construir la tabla
-        table_data = []
+        table = ttk.Treeview(self, columns=table_header, show="headings")
+        for header in table_header:
+            table.heading(header, text=header)
+            table.column(header, width=155, anchor="center") 
+
         for resultado in resultados:
-            fila = [resultado['ID'], resultado['Expresión Regular'], resultado['Cadena'], resultado['Cumple']]
-            table_data.append(fila)
+            table.insert("", "end", values=(resultado['ID'], resultado['Expresión Regular'], resultado['Cadena'], resultado['Cumple']))
 
-        # Longitud máxima de cada columna para ajustar el ancho de la tabla
-        column_widths = [max(len(str(row[i])) for row in table_data) for i in range(len(table_header))]
+        # Mostrar la tabla en lugar de self.output_text
+        table.grid(row=4, column=1, padx=10, pady=5, sticky="nsew")
 
-        # Construir la tabla con formato
-        table = ""
-        table += "|".join(f"{header.ljust(column_widths[i])}" for i, header in enumerate(table_header)) + "\n"
-        table += "|".join("-" * width for width in column_widths) + "\n"
-        for row in table_data:
-            table += "|".join(f"{str(item).ljust(column_widths[i])}" for i, item in enumerate(row)) + "\n"
+    def generar_arbol_derivacion(self):
+        input_text_value = self.input_text.get(1.0, tk.END)
+        analizador_expresiones = Analizador_expresiones()
+        analizador = Analizador()
+        tokens_lexemas, caracteres_no_permitidos = analizador.analizador_lexico(input_text_value)
+        tokens_a_analizar = [token for token in tokens_lexemas if token.name not in ['COMENTARIO_LINEA', 'COMENTARIO_MULTILINEA']]
+        expresiones_cadena = analizador_expresiones.obtener_expresiones_cadenas(tokens_a_analizar)
+        parser = Parser(tokens_a_analizar)
+        error_sintacticos = parser.parse()
+        if caracteres_no_permitidos or error_sintacticos:
+            texto_error = ''
+            for error in caracteres_no_permitidos:
+                if error[0].isalpha():
+                    texto_error += f'Identificador no válido: {error[0]}. Línea: {error[1]}. Columna: {error[2]}\n'
+                else:
+                    texto_error += f'Caracter no permitido: {error[0]}. Línea: {error[1]}. Columna: {error[2]}\n'
+            
+            texto_error_sintáctico = 'Errores sintácticos:\n'
+            for error in error_sintacticos:
+                texto_error_sintáctico += f'Error: {error.value}. Línea: {error.line}. Columna: {error.column}\n'
+        
+            self.output_text = tk.Text(self, height=35, width=88, highlightbackground='gray90', highlightthickness=2, relief=tk.FLAT)
+            self.output_text.grid(row=4, column=1, padx=10, pady=5)
+            self.output_text.delete(1.0, tk.END)
+            self.output_text.insert(tk.END, f'No se pudo analizar el archivo.\n{texto_error} \n{texto_error_sintáctico}')
+        else:
+            with open('arbol_derivacion.dot', 'w') as dot_file:
+                dot_file.write(self.generar_dot_arbol_derivacion(expresiones_cadena))
 
-        # Mostrar la tabla en self.output_text
-        self.output_text.delete(1.0, tk.END)
-        self.output_text.insert(tk.END, table)
+            tk.messagebox.showinfo("Árbol de Derivaciones", "El árbol de derivaciones se ha generado exitosamente en formato DOT.")
 
-    def generar_grafo(self):
+    def generar_dot_arbol_derivacion(self,expresiones_cadena):
         dot_code = """
         digraph G {
             node [shape=circle, fontsize=10];
-            rankdir=LR;
-
-            // Estados
-            q0 [shape=circle]; 
-            q1 [shape=doublecircle]; 
-            q2 [shape=doublecircle];
-            q3 [shape=circle]; 
-            q4 [shape=circle];
-            q5 [shape=doublecircle];
-
-            // Transiciones
-            q0 -> q1 [label="a-zA-Z"];
-            q1 -> q1 [label="a-zA-Z"];
-            q0 -> q2 [label="[ ]{ }:;,="]
-            q0 -> q3 [label="''"]
-            q3 -> q4 [label="a-zA-Z"]
-            q4 -> q4 [label="a-zA-Z"]
-            q4 -> q2 [label="''"]
-            q0 -> q5 [label="0...9"]
-        }
+            rankdir=TB;
         """
-        graph = graphviz.Source(dot_code)
-        graph.render('DFA', format='png', cleanup=True)
+
+        for expresion in expresiones_cadena:
+            dot_code += f"\n\n    // Expresion Regular: {expresion['ER']}\n"
+            dot_code += f"    q0 [shape=circle];\n"
+            dot_code += f"    q{len(expresion['ER'])} [shape=doublecircle];\n"
+
+            for i in range(len(expresion['ER'])):
+                dot_code += f"    q{i+1} [shape=circle];\n"
+                label = expresion['ER'][i]
+                label = label.replace('"', '\\"')
+                label = label.replace('á', '\\u00e1').replace('é', '\\u00e9').replace('í', '\\u00ed').replace('ó', '\\u00f3').replace('ú', '\\u00fa')
+                dot_code += f"    q{i} -> q{i+1} [label=\"{label}\"];\n"
+
+        dot_code += "\n}"
+        return dot_code
+
+
+
 
